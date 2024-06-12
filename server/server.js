3f45c82239b2eb097e780e48e4bf80f4baeaa572
HuyBV02 <bvhuy120202@gmail.com>
2024-06-12 16:37:29 +0700

    initial server

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "X-Requested-With",
+            "Origin",
+            "Accept",
+            "Access-Control-Allow-Origin",
+        ],
+        exposedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "X-Requested-With",
+            "Origin",
+            "Accept",
+            "Access-Control-Allow-Origin",
+        ],
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+    })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`:::::::::::::Server is running on port ${port}:::::::::::::`);
+});
